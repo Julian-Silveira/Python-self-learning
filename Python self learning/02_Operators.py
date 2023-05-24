4b80f2672bcff7e9583e163b76ed18536538a602
Julian Silveira <juliantadeosilveira@gmail.com>
2023-05-24 01:22:39 -0300

    modified:   Python self learning/02_Operators.py

--- Python self learning/02_Operators.py
+++ Python self learning/02_Operators.py
@@ -1,7 +1,14 @@
 ### Operadores ### 
+
 print( 3 + 4) ## igual 7 
 print( 3 - 4) ## igual -1
 print( 3 * 4) ## igual 12
 print( 3 / 4) ## 0.75 
+print(10 % 3) ## es el modulo
+print(10 // 3) ## Float division, siempre da un numero entero aproximado.
+print(2 ** 3) ## Calcular el exponente ( Dos elevado a tres )
 
-## Quede en el minuto 1:43
+print("Hola " + "Python" + ", Que tal?") ## Los operadores pueden tambien funcionar con variables aunque de manera limitad (no se pueden restar strings)
+print("Hola " + str(5)) ## Definir varial numerica como tipo string, el dato se transforma
+print("Hola " * 5) # Multiplicar strings
+print("Hola " * (2 * 3)) # Operar con operadores 
